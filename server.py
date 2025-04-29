469fc99a530e9b204a655849cc675d37905f08a0
Warren <whfu2004@gmail.com>
2025-04-28 21:15:03 -0600

    updated dupes

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
 from flask import Flask, request
 from flask_cors import CORS
 from bs4 import BeautifulSoup
+import torch
+import torch.nn as nn
+
+"""
+DEFINE NN MODEL:
+"""
+sensitivity = 0.8  # Adjust sensitivity as needed
+model = torch.load('models/model.pkl')
+
 
 app = Flask(__name__)
 CORS(app)  # Allow Chrome extension to call us
 
 @app.route('/process', methods=['POST'])
 def process_html():
+    global sensitivity, model
     data = request.get_json()
     html = data.get('html', '')
 
@@ -27,6 +37,7 @@ def process_html():
         """
         CALL NN
         """
+        model.
         # if any('ad' in c.lower() for c in class_list):
         #     divs_to_remove.append(tag)
     for c in list(set(classes)):
